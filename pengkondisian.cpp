2a18943f93bfb579dc507763b09f37647d3ce6df
rizkanfirmansyah <riezkanaprianda@gmail.com>
2021-11-24 13:57:52 +0700

    update

--- pengkondisian.cpp
+++ pengkondisian.cpp
@@ -12,4 +12,25 @@ int main(){
     }
 
     printf("\n\n");
+
+      int nilai = 69;
+    printf("nilai : %d\n", nilai);
+
+    if (nilai >= 95) {
+        printf("indeks A");
+    } else if (nilai >= 80){
+        printf("indeks B");
+    } else if (nilai >= 70){
+        printf("indeks B-");
+    } else if (nilai >= 75){
+        printf("indeks c");
+    } else if (nilai >= 70){
+        printf("indeks C-");
+    } else if (nilai >= 60){
+        printf("indeks D");
+    } else if (nilai >= 40){
+        printf("indeks E");
+    } else {
+        printf("indeks tidak diketahui");
+    }
 }
