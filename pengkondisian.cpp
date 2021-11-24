2ba653a5242c6472f954bd2274ccc31d5a485c9c
rizkanfirmansyah <riezkanaprianda@gmail.com>
2021-11-24 13:55:31 +0700

    update pengkondisian

---
+++ pengkondisian.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    bool is_success = false;
+    printf("is_success : %d\n", is_success);
+
+    if(is_success){
+        printf("hasilnya sukses");
+    }else{
+        printf("hasilnya gagal!");
+    }
+
+    printf("\n\n");
+}
