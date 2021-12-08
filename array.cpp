7e192ef7494b8f79d49bf6d0015522ffed30331d
rizkanfirmansyah <riezkanaprianda@gmail.com>
2021-12-08 13:59:49 +0700

    update array c plus plus

--- array.cpp
+++ array.cpp
@@ -19,4 +19,39 @@ int main(){
     printf("%d \n", deret2[3]);
     printf("\n\n");
 
+    float deret3[4];
+    deret3[0] = 30.1;
+    deret3[1] = 30.2;
+    deret3[2] = 30.3;
+    deret3[3] = 30.4;
+    printf("%.2f \n", deret3[0]);
+    printf("%.2f \n", deret3[1]);
+    printf("%.2f \n", deret3[2]);
+    printf("%.2f \n", deret3[3]);
+    printf("\n\n");
+
+        char kata1[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+    printf("%c \n", kata1[0]);
+    printf("%c \n", kata1[2]);
+    printf("%c \n", kata1[4]);
+    printf("%c \n", kata1[6]);
+    printf("%s \n", kata1);
+    printf("\n\n");
+
+    char kata2[20] = {'i', 'n', 'i', ' ', 'a', 'd', 'a', 'l', 'a', 'h', ' ', 'k', 'a', 't', 'a'};
+    printf("%c \n", kata2[0]);
+    printf("%c \n", kata2[1]);
+    printf("%c \n", kata2[2]);
+    printf("%c \n", kata2[3]);
+    printf("%s \n", kata2);
+    printf("\n\n");
+
+    char kata3[20];
+    kata3[0] = 'i';
+    kata3[1] = 'n';
+    kata3[2] = 'i';
+    kata3[3] = ' ';
+    printf("%s \n", kata3);
+    printf("\n\n");
+
 }
